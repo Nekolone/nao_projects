524adac188f441013686e1398ede0d7584edd75d
Nikolai Kottcov <nikolafgh@gmail.com>
2021-07-05 16:12:07 +0300

    qwe

--- src/vision/visionRecognition.py
+++ src/vision/visionRecognition.py
@@ -1,4 +1,6 @@
 # -*- encoding: UTF-8 -*-
+
+
 import time
 import qi
 import vision_definitions
@@ -9,30 +11,42 @@ from PIL import Image
 import cv2
 import numpy as np
 
+from naoqi import ALModule, ALBroker
 
+
+
+
+
+
+class ColorDetector(ALModule):
+    """
+    EBANIY TI ABEMA
+    """
+    def __init__(self, name, color = "red", diameter = 1):
+        self.colors = {"red":[255 - 15, 30, 30, 50],"blue":[30, 30, 255 - 15, 50],"yellow":[255 - 15, 255 - 15, 30, 50]}
+        self.name = name
+        ALModule.__init__(self, self.name)
+
+        if color not in ["red","blue","yellow"]:
+            raise Exception ("suka, bil bi ti chelovekom, EBANIY TI ABEMA")
+        blob.setColor(*self.colors[color])
+        blob.setObjectProperties(20, diameter, "Circle")
+        memory.subscribeToEvent("ALTracker/ColorBlobDetected", self.name, "onBlobDetection")
+        global videoClient
+        videoClient = video.subscribeCamera("myCam",0,3,11,5)
+
+
+    # @staticmethod
+    def onBlobDetection(self, eventName, value, subscriberIdentifier):
+        """
+        EBANIY TI ABEMA, TVOU DOCHKU
+        """
+        print "AHH, i see them"
 
 
 def main():
-    global tts, memory, video
-    
-    tts = session.service( "ALTextToSpeech" )
-    video = session.service( "ALVideoDevice" ) 
-    # vr = session.service("ALVisionRecognition")   
-    memory = session.service( "ALMemory" )
-    # cbd = session.service("ALColorBlobDetection")
-    # tracker = session.service( "ALTracker" )
-    # motion = session.service("ALMotion")
-    # posture = session.service("ALRobotPosture")
-    # wr = session.service("ALWorldRepresentation")
 
-    # resolution = vision_definitions.kQQVGA
-    # colorSpace = vision_definitions.kYUVColorSpace
-
-    # resolution = 2
-    # colorSpace = 11
-    # fps = 20
-
-    videoClient = video.subscribeCamera("myCam",0,1,11,20)
+    videoClient = video.subscribeCamera("myCam",0,4,11,1)
     naoImage = video.getImageRemote(videoClient)
     video.unsubscribe(videoClient)
 
@@ -41,51 +55,156 @@ def main():
     array = naoImage[6]
     image_string = str(bytearray(array))
     im = Image.frombytes("RGB", (imageWidth, imageHeight), image_string)
-    im.save("camImage.png", "PNG")
-
-    img = cv2.imread("C:\\Users\\tsi_nao\\Desktop\\nao_projects\\camImage.png", cv2.IMREAD_COLOR)
-
-    cv2.imshow("Input Image", img)
-    cv2.waitKey(0)
+    im.save("camImage.jpg", "JPEG")
 
 
-    params = cv2.SimpleBlobDetector_Params()
+    # global bub, bub2
+    # bub = ColorDetector("bub","red")
+    # time.sleep(5)
+    # blob.setColor(30, 30, 255 - 15, 50)
 
-    params.filterByColor = 0
-    params.blobColor = 0
-    params.filterByArea = 1
-    params.minArea = 0
-    params.maxArea = 10000
-    params.filterByCircularity=0
-    params.filterByConvexity  =0
-    params.filterByInertia  =0
+    # try:
+    #     while True:
+    #         print " "
+    #         time.sleep(1)
+    # except KeyboardInterrupt:
+    #     print "Interrupted by user, stopping"
+    #     # vr.unsubscribe("ALTracker/ColorBlobDetected")
+    #     #stop
+    #     video.unsubscribe(videoClient)
+    #     # sys.exit(0)
 
-
-
-
-
-    detector = cv2.SimpleBlobDetector_create()
-    keyppoint_info = detector.detect(img)
-    blank_img = np.zeros((1,1))
-    blobs = cv2.drawKeypoints(img, keyppoint_info, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-
-    cv2.imshow("displaying blobs", blobs)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
-    
+global video, memory, blob, broker
 
 
-ip = "192.168.253.155"
-# ip = "192.168.252.62"
-# ip = "192.168.252.226"
+ip = "192.168.252.226"
 # ip = "192.168.252.62"
 
 session = qi.Session()
 session.connect("tcp://" + ip + ":" + "9559")
+video = session.service( "ALVideoDevice" )   
+memory = session.service( "ALMemory" )
+blob = session.service("ALColorBlobDetection")
+broker = ALBroker("pythonMotionBroker", "0.0.0.0", 0, ip, 9559)
 
 
 main()
+# import time
+# import qi
+# import vision_definitions
+
+# import PIL
+# from PIL import Image
+
+# import cv2
+# import numpy as np
+
+
+# class (ALModule)
+
+
+# def main():
+#     global tts, memory, video
+    
+#     tts = session.service( "ALTextToSpeech" )
+#     video = session.service( "ALVideoDevice" ) 
+#     # vr = session.service("ALVisionRecognition")   
+#     memory = session.service( "ALMemory" )
+#     cbd = session.service("ALColorBlobDetection")
+#     # tracker = session.service( "ALTracker" )
+#     # motion = session.service("ALMotion")
+#     # posture = session.service("ALRobotPosture")
+#     # wr = session.service("ALWorldRepresentation")
+
+#     # resolution = vision_definitions.kQQVGA
+#     # colorSpace = vision_definitions.kYUVColorSpace
+
+#     # resolution = 2
+#     # colorSpace = 11
+#     # fps = 20
+
+#     videoClient = video.subscribeCamera("myCam",0,3,11,5)
+#     naoImage = video.getImageRemote(videoClient)
+#     # video.unsubscribe(videoClient)
+
+#     imageWidth = naoImage[0]
+#     imageHeight = naoImage[1]
+#     array = naoImage[6]
+#     image_string = str(bytearray(array))
+#     im = Image.frombytes("RGB", (imageWidth, imageHeight), image_string)
+
+#     cbd.setColor(255-15,0,0, 255)
+#     cbd.setObjectProperties(10, 0.1, "Unknown")
+#     memory.subscribeToEvent("ALTracker/ColorBlobDetected", "BallDetector", "onBlobDetection")
+#     print("BallDetector initialized!")
+#     # threading.Lock()
+
+#     got_color = False
+
+#     try:
+#         while True:
+#             print "helpme"
+#             time.sleep(1)
+#     except KeyboardInterrupt:
+#         print "Interrupted by user, stopping"
+#         # vr.unsubscribe("ALTracker/ColorBlobDetected")
+#         #stop
+#         video.unsubscribe(videoClient)
+#         sys.exit(0)
+
+
+# def onBlobDetection(eventName, value, subscriberIdentifier):
+#     print "HELPMEEEE"
+#     positionInFrameRobot = value[1]
+#     print("Ball detected.")
+#     timestampUS = value[2][1]
+#     tts.say("o ball")
+
+#     # im.save("camImage.png", "PNG")
+
+#     # img = cv2.imread("C:\\Users\\tsi_nao\\Desktop\\nao_projects\\camImage.png", cv2.IMREAD_GRAYSCALE)
+
+#     # cv2.imshow("Input Image", img)
+#     # cv2.waitKey(0)
+
+
+#     # params = cv2.SimpleBlobDetector_Params()
+
+#     # params.filterByColor = 1
+#     # params.blobColor = 255
+#     # params.filterByArea = 1
+#     # params.minArea = 0
+#     # params.maxArea = 10000
+#     # params.filterByCircularity=0
+#     # params.filterByConvexity  =0
+#     # params.filterByInertia  =0
+
+
+
+
+
+#     # detector = cv2.SimpleBlobDetector_create()
+#     # keyppoint_info = detector.detect(img)
+#     # blank_img = np.zeros((1,1))
+#     # blobs = cv2.drawKeypoints(img, keyppoint_info, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+#     # cv2.imshow("displaying blobs", blobs)
+#     # cv2.waitKey(0)
+#     # cv2.destroyAllWindows()
+
+    
+
+
+# # ip = "192.168.253.155"
+# # ip = "192.168.252.62"
+# ip = "192.168.252.226"
+# # ip = "192.168.252.62"
+
+# session = qi.Session()
+# session.connect("tcp://" + ip + ":" + "9559")
+
+
+# main()
 
 
 
@@ -101,7 +220,7 @@ main()
 
 
 #     cbd.setColor(255-15,0,0, 255)
-#     cbd.setObjectProperties(10, 0.1, "Circle")
+#     cbd.setObjectProperties(10, 0.1, "Unknown")
 #     memory.subscribeToEvent("ALTracker/ColorBlobDetected", "colorDetector", "onBlobDetection")
 #     print("BallDetector initialized!")
 #     threading.Lock()
